f8a1cdfada5d550cd01c7b29fe4a38f3577a2719
José Antonio <joseantonio.monino@murciaeduca.es>
2024-10-17 10:27:07 +0000

    getAll

---
+++ todo.php
@@ -0,0 +1,42 @@
+<?php
+
+class Todo implements \JsonSerializable {
+    private int $item_id;
+	private string $content;
+	
+    public function parametersConstruct(int $item_id, string $content){
+        $this->item_id = $item_id;
+        $this->content = $content;
+    }
+
+	public function jsonConstruct($json) {
+        foreach (json_decode($json, true) AS $key => $value) {
+            $this->{$key} = $value;
+        }
+    }
+
+	public function jsonSerialize()
+    {
+        $vars = get_object_vars($this);
+        return $vars;
+    }
+	
+	public function getContent(){		
+		return $this->content;	
+	}
+	
+	public function getItem_id(){		
+		return $this->item_id;
+	}
+
+    public static function DB_selectAll($dbconn){
+        $todo_list = array();
+        foreach($dbconn->query("SELECT item_id, content FROM todo_list") as $row) {
+            $new_todo = new Todo;
+            $new_todo->parametersConstruct($row['item_id'],$row['content']);
+            $todo_list[]=$new_todo;
+        }
+
+
+    }
+}
