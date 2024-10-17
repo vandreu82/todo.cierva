8feed6c0722ed207df3362a9802809bcc7cde4b9
José Antonio <joseantonio.monino@murciaeduca.es>
2024-10-17 09:48:49 +0000

    init

---
+++ DB.php
@@ -0,0 +1,16 @@
+<?php
+
+class DB {
+    public $connection;
+    public function __construct() {
+        $db_host        = "127.0.0.1";
+        $db_name      = "example_database";
+        $db_user = "example_user2";
+        $db_pass = "password";
+        $this->connection = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);;
+    }
+
+    public function __destruct() {
+        $this->connection = null;
+    }
+}
