4aea78e358ede42be01834ddac11ca9ace2519c3
vandreu82 <vmaf82@gmail.com>
2024-11-11 23:03:13 +0000

    Añadidas credenciales de mysql y un favicon por la cara. Corregido el problema del CORS.
    Changes to be committed:
    	modified:   DB.php
    	modified:   controller.php
    	new file:   favicon.ico

--- DB.php
+++ DB.php
@@ -4,9 +4,9 @@ class DB {
     public $connection;
     public function __construct() {
         $db_host = "127.0.0.1";
-        $db_name = "example_database";
-        $db_user = "example_user2";
-        $db_pass = "password";
+        $db_name = "todo";
+        $db_user = "todo";
+        $db_pass = "todo_password123";
         $this->connection = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);;
     }
 
--- controller.php
+++ controller.php
@@ -1,40 +1,50 @@
+<?php
+header("Access-Control-Allow-Origin: http://todo.terminus.lan");
+header("Access-Control-Allow-Methods: POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
-	<?php
-			require "todo.php";
-			require "DB.php";
-		
-		
-			function return_response($status, $statusMessage, $data) {
-				header("HTTP/1.1 $status $statusMessage");
-				header("Content-Type: application/json; charset=UTF-8");
-				echo json_encode($data);
-			}
-			 
-			$bodyRequest = file_get_contents("php://input");
-			
-			switch ($_SERVER['REQUEST_METHOD']) {
-				case 'POST':
-					$db = new DB();
-					$new_todo = new Todo;
-					$new_todo->jsonConstruct($bodyRequest);
-					$new_todo->DB_insert($db->connection); 
-					$todo_list = Todo::DB_selectAll($db->connection);
-					return_response(200, "OK", $todo_list);
-					break;
-				case 'DELETE':
-					$db = new DB();
-					$new_todo = new Todo;
-					$new_todo->jsonConstruct($bodyRequest);
-					$new_todo->DB_delete($db->connection); 
-					$todo_list = Todo::DB_selectAll($db->connection);
-					return_response(200, "OK", $todo_list);
-					break;
-				default:
-					return_response(405, "Method Not Allowed", null);
-					break;
-			}
-	
-		
-		
-	?>
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+	// Termina la ejecución para las solicitudes de verificación (OPTIONS) de CORS
+	exit(0);
+}
 
+
+
+require "todo.php";
+require "DB.php";
+
+
+function return_response($status, $statusMessage, $data)
+{
+	header("HTTP/1.1 $status $statusMessage");
+	header("Content-Type: application/json; charset=UTF-8");
+	echo json_encode($data);
+}
+
+$bodyRequest = file_get_contents("php://input");
+
+switch ($_SERVER['REQUEST_METHOD']) {
+	case 'POST':
+		$db = new DB();
+		$new_todo = new Todo;
+		$new_todo->jsonConstruct($bodyRequest);
+		$new_todo->DB_insert($db->connection);
+		$todo_list = Todo::DB_selectAll($db->connection);
+		return_response(200, "OK", $todo_list);
+		break;
+	case 'DELETE':
+		$db = new DB();
+		$new_todo = new Todo;
+		$new_todo->jsonConstruct($bodyRequest);
+		$new_todo->DB_delete($db->connection);
+		$todo_list = Todo::DB_selectAll($db->connection);
+		return_response(200, "OK", $todo_list);
+		break;
+	default:
+		return_response(405, "Method Not Allowed", null);
+		break;
+}
+
+
+
+?>
